bcf6ae1de6337385b1bfcb538445f7f44db275ac
s0lvang <august.s.solvang@gmail.com>
2020-09-11 16:44:21 +0200

    move to darknet-wrapper for object-detection

--- yolo.py
+++ yolo.py
@@ -1,19 +1,43 @@
+import time
+
+import pydarknet
+from pydarknet import Detector, Image
 import cv2
-from predictAndDrawBbox import predict_and_draw_boxes
 
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
+if __name__ == "__main__":
+    # Optional statement to configure preferred GPU. Available only in GPU version.
+    # pydarknet.set_cuda_device(0)
 
-if vc.isOpened():  # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+    net = Detector(bytes("cfg/yolo-tiny.cfg", encoding="utf-8"), bytes("weights/yolov3-tiny.weights", encoding="utf-8"), 0,
+                   bytes("cfg/coco.data", encoding="utf-8"))
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.retrieve()
-    frame = predict_and_draw_boxes(frame)
-    key = cv2.waitKey(20)
-    if key == 27:  # exit on ESC
-        break
-cv2.destroyWindow("preview")
+    cap = cv2.VideoCapture(-1)
+
+    while True:
+        r, frame = cap.read()
+        if r:
+            start_time = time.time()
+
+            # Only measure the time taken by YOLO and API Call overhead
+
+            dark_frame = Image(frame)
+            results = net.detect(dark_frame)
+            del dark_frame
+
+            end_time = time.time()
+            # Frames per second can be calculated as 1 frame divided by time required to process 1 frame
+            fps = 1 / (end_time - start_time)
+
+            print("FPS: ", fps)
+            print("Elapsed Time:",end_time-start_time)
+
+            for cat, score, bounds in results:
+                x, y, w, h = bounds
+                cv2.rectangle(frame, (int(x-w/2),int(y-h/2)),(int(x+w/2),int(y+h/2)),(255,0,0))
+                cv2.putText(frame, str(cat.decode("utf-8")), (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
+
+            cv2.imshow("preview", frame)
+
+        k = cv2.waitKey(1)
+        if k == 0xFF & ord("q"):
+            break
